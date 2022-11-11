c6b95080d2da09e64d663f9c683d614eebba580a
Can Demirtas <can@memfault.com>
2022-11-11 18:12:51 +0100

    Several typo fixes and improvements to the images

--- sidebars.js
+++ sidebars.js
@@ -118,7 +118,7 @@ module.exports = {
                 "android/android-releases-integration-guide",
                 "android/android-ota-update-client",
                 "android/android-data-scrubbing",
-                // "android/android-multi-device-support",
+                "android/android-multi-device-support",
             ],
         },
         {
